8f1170982ce2b7bee73d8362b581250c82502da5
Holly <145540630+HollyintheWorld@users.noreply.github.com>
2023-09-21 23:33:47 +1000

    GUICalculatorExample.py

--- GUICalculatorExample.py
+++ GUICalculatorExample.py
@@ -19,7 +19,7 @@ def reset():
 
 window = Tk() 
 
-l1 = Label(window, text="현재 합계")
+l1 = Label(window, text="current sum")
 l1.grid(row=0, column=0)
 
 l2 = Label(window, text=sum)
@@ -28,13 +28,13 @@ l2.grid(row=0, column=1)
 e1 = Entry(window)
 e1.grid(row=1, column=0, columnspan=3)
 
-b1 = Button(window,text="더하기 (+)", command=plus)
+b1 = Button(window,text="Plus (+)", command=plus)
 b1.grid(row=2,column=0)
 
-b2 = Button(window,text="빼기(-)", command=minus)
+b2 = Button(window,text="Minus (-)", command=minus)
 b2.grid(row=2,column=1)
 
-b3 = Button(window,text="초기화",command=reset)
+b3 = Button(window,text="Initialize the sum",command=reset)
 b3.grid(row=2,column=2)
 
 window.mainloop()
